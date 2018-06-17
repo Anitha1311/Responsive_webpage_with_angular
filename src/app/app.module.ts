40c46037709e25a83abe13b6d244df3f22fa4610
karthikeyan m <mlanitha13@gmail.com>
2018-06-17 19:54:15 +0530

    Completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { LandingPageComponent } from './pages/landing-page/landing-page.component';
+import { TileComponent } from './tile/tile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LandingPageComponent,
+    TileComponent
   ],
   imports: [
     BrowserModule
